58090feb0c46c44d76f3b3a5aba45c6e6d5800ee
Chance Harmon <henrikillsit@gmail.com>
2020-02-14 13:05:54 -0800

    single linked list class finished

--- dataStructures/singleLinkedList/singleLinkedListClass.js
+++ dataStructures/singleLinkedList/singleLinkedListClass.js
@@ -9,6 +9,11 @@ class Node {
   }
 }
 
+//Traverseing 
+// while(current){
+//   current = current.next;
+// }
+
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -27,4 +32,115 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let previous = current;
+    while (current.next) {
+      previous = current;
+      current = current.next;
+    }
+    this.tail = previous;
+    this.tail.next = null;
+    this.length--;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  //removes from beginning
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  //Adds to the beginning;
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return null;
+    if (index === this.lenght) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let previous = this.get(index - 1);
+    let temp = previous.next;
+    previous.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let previous = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = previous;
+      previous = node;
+      node = next;
+    }
+    return this;
+  }
+  //Helper method to be able to see if the reverse method is working, not required for the class itself
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val)
+      current = current.next
+    }
+    console.log(arr);
+  }
 }
+
+//Big O
+//Insertion = O(1)
+//Remove = O(1) if removing from beginning, or O(n)
+//Search = O(n)
+//Access = O(n)
