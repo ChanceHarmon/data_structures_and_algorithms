c4562c5d09147d7272122ef8060ec8c1f1a469fe
Chance Harmon <henrikillsit@gmail.com>
2020-02-14 17:33:16 -0800

    working on double linked list class

--- dataStructures/doubleLinkedList/doubleLinkedListClass.js
+++ dataStructures/doubleLinkedList/doubleLinkedListClass.js
@@ -1,1 +1,74 @@
 'use strict';
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return null;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this, tail = null;
+    } else {
+      this.tail = poppedNode.previous;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    if (!this.head) return null;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.previous = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift() {
+    let newNode = new Node(val);
+    if (!this.head === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.previous = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+
+  }
+}
