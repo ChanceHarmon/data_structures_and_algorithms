d136a90c42c5ba881321b5ecbf466dc1f111c27d
Chance Harmon <henrikillsit@gmail.com>
2020-03-03 12:47:23 -0800

    merge two sorted linked list complete

---
+++ top100AmazonLeetCode/easy/mergeTwoSortedLists.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+// Example:
+
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+//Big O space: O(n), we will have as many stack frames as the two lists have combined elements, before we ever return
+//Big O time: O(n) beacuse we will exactly the number of recursive calls we we have elements in the two lists
+
+var mergeTwoLists = function (l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  //we need to compare first values in each list
+  // this way we know that the list we are creating in place will be properly sorted after the merge
+  //recursivly checking and calling it self until either list is empty, if one is empty we know the rest of the other list is sorted by default, because the lists were sorted when they were given to us
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+
+};
---
+++ top100AmazonLeetCode/easy/twoSum.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//Big O space: O(n) as we are constructing a hash map
+//Big O time: O(n), constant lookup to the hash map, and only looping once
+
+var twoSum = function (nums, target) {
+  //initializing hash map
+  const hashMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    //initializing result variable
+    const result = target - nums[i];
+    //checking if the result is in the hash map, then we are returning
+    // the two indicies
+    if (hashMap.has(nums[i])) {
+      return [hashMap.get(nums[i]), i];
+    } else {
+      //else we are adding it to the hash map
+      hashMap.set(result, i);
+    }
+  }
+};
