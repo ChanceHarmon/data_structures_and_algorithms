3dd3eeaf9f1910acb778e96253d927db2b14e469
Chance Harmon <henrikillsit@gmail.com>
2021-01-05 15:15:13 -0800

    new leetcode question

---
+++ leetCode/easy/removeDuplicates.js
@@ -0,0 +1,60 @@
+'use strict'
+
+
+// Remove Duplicates from Sorted Array
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Clarification:
+
+// Confused why the returned value is an integer but your answer is an array?
+
+// Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+// Internally you can think of this:
+
+// // nums is passed in by reference. (i.e., without making a copy)
+// int len = removeDuplicates(nums);
+
+// // any modification to nums in your function would be known by the caller.
+// // using the length returned by your function, it prints the first len elements.
+// for (int i = 0; i < len; i++) {
+//     print(nums[i]);
+// }
+
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2]
+// Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4]
+// Explanation: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.
+
+
+// Constraints:
+
+// 0 <= nums.length <= 3 * 104
+// -104 <= nums[i] <= 104
+// nums is sorted in ascending order.
+
+//Big O time: O(n) Worst case there are no duplicates, and the loop has to run the enite length of the array.
+//Big O space: O(1) We do create a variable to store a count, but respectively this is not considered extra storage to increase space capacity.
+
+
+const removeDuplicates = nums => {
+  if (nums.length === 0) return 0;
+  let count = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[count]) {
+      count++;
+      nums[count] = nums[i];
+    }
+  }
+  return count + 1;
+};
